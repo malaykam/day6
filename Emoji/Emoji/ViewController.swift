a5d88739925c3b6a393255f5c928b3cc97565a2b
Malayka <malayka@Apples-MacBook-Pro-35.local>
2019-08-12 10:12:24 -0400

    iteration 0

---
+++ Emoji/Emoji/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emoji
+//
+//  Created by Malayka on 8/12/19.
+//  Copyright © 2019 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
